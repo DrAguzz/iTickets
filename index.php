920e873d9ea79c58f34e45ce0a0712e90214bc37
Erfan Bagus <erfanbagus06@gmail.com>
2026-01-03 04:43:07 +0800

    .htaccess

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
- header("Location:./frontend/login.php");
+ $path = $_SERVER['REQUEST_URI'];
+
+ if($path == "/"){
+    include "frontend/login.php";
+ }elseif($path == "/admin"){
+    include "./admin/index.php";
+ }else{
+    echo "Not Found !!  Please double check your address again";
+ }
+
 ?>
